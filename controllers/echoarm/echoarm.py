bfefafb24e433340732f9570f4ad8a31322e0146
andrebellu <bellu.andrea@outlook.it>
2025-10-27 13:35:20 +0100

    added new meshes - started proto in webots

--- controllers/echoarm/echoarm.py
+++ controllers/echoarm/echoarm.py
@@ -1,32 +1,43 @@
 from controller import Robot
 
 robot = Robot()
-
 timestep = int(robot.getBasicTimeStep())
 
-base_motor = robot.getDevice("base_arm_joint")
-base_sensor = robot.getDevice("base_arm_joint_sensor")
-arm_motor = robot.getDevice("arm_joint")
-arm_sensor = robot.getDevice("arm_joint_sensor")
+MAX_SLIDER_POSITION = 0.120
+MIN_SLIDER_POSITION = 0.0
+velocity_slider = 0.5
 
-base_sensor.enable(timestep)
-arm_sensor.enable(timestep)
+MAX_ELBOW_POSITION = 6.28 # 2pi
+MIN_ELBOW_POSITION = 0.0
+velocity_elbow = 1.0
 
-base_motor.setPosition(float('inf'))
-base_motor.setVelocity(0.0)
-arm_motor.setPosition(float('inf'))
-arm_motor.setVelocity(0.0)
+slider_piston_base = robot.getDevice("slider_piston_base")
+slider_piston_base.setPosition(float('inf'))
+slider_piston_base.setVelocity(velocity_slider)
+ps_slider = robot.getDevice("slider_piston_base_sensor") 
+ps_slider.enable(timestep)
 
-base_velocity = 1.0    # rad/s
-arm_velocity = 1.0     # rad/s
+piston_elbow_motor = robot.getDevice("piston_elbow_motor")
+piston_elbow_motor.setPosition(float('inf'))
+piston_elbow_motor.setVelocity(velocity_elbow) 
 
-base_motor.setVelocity(base_velocity)
-arm_motor.setVelocity(arm_velocity)
+ps_elbow = robot.getDevice("piston_elbow_sensor") 
+ps_elbow.enable(timestep)
 
-print("Avvio controller per rotazione continua...")
+print("Avvio controller coordinato...")
 
 while robot.step(timestep) != -1:
-    base_pos = base_sensor.getValue()
-    arm_pos = arm_sensor.getValue()
     
-    print(f"Base joint: {base_pos:.3f} rad | Arm joint: {arm_pos:.3f} rad")
+    current_slider_pos = ps_slider.getValue()
+    
+    if current_slider_pos > MAX_SLIDER_POSITION:
+        slider_piston_base.setVelocity(-velocity_slider)
+    elif current_slider_pos < MIN_SLIDER_POSITION:
+        slider_piston_base.setVelocity(velocity_slider)
+
+    current_elbow_pos = ps_elbow.getValue() 
+    
+    if current_elbow_pos > MAX_ELBOW_POSITION:
+        piston_elbow_motor.setVelocity(-velocity_elbow)
+    elif current_elbow_pos < MIN_ELBOW_POSITION:
+        piston_elbow_motor.setVelocity(velocity_elbow)
